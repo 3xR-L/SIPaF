c1c61c80590914d0c4ace0024b1980cbf90b629c
Raul Ruben Romero Lopez <101576450+3xR-L@users.noreply.github.com>
2022-12-05 16:00:29 -0600

    Agregar archivo de análisis y diseño.

--- controlador/CEDT.py
+++ controlador/CEDT.py
@@ -20,13 +20,19 @@ class CEDT(qtw.QMainWindow):
         print(self.CGEDT.mEDT.direccion)
         model = QFileSystemModel()
         model.setRootPath(QDir.currentPath())
-        self.ui.vistaArbolProyectos.setModel(model)
-        self.ui.vistaArbolProyectos.setRootIndex(model.index(self.CGEDT.mEDT.direccion+'/'+self.CGEDT.mEDT.nombreEDT))
+        self.ui.vista_arbol_proyectos.setModel(model)
+        self.ui.vista_arbol_proyectos.setRootIndex(model.index(self.CGEDT.mEDT.direccion+'/'+self.CGEDT.mEDT.nombreEDT))
 
     def clicks(self):
-        # Open VGestionarEDT when the action cambiaar on the menu EDT
-        self.ui.menubar.triggered.connect(self.openVCGestionarEDT)
+        # Open VGestionarEDT when the action cambiar on the menu EDT if nuevo, guardar, eliminar or cambiar is clicked
+        # on the EDT menu
+        self.ui.EDT_nuevo.triggered.connect(self.openVCGestionarEDT)
+        self.ui.EDT_cambiar.triggered.connect(self.openVCGestionarEDT)
+        self.ui.EDT_eliminar.triggered.connect(self.openVCGestionarEDT)
 
+        #self.ui.menubar.triggered.connect(self.openVCGestionarEDT)
+
+        # Open VDatos Princiaples del proyecto
     def openVCGestionarEDT(self):
         self.CGEDT.openVGestionarEDT()
         print('openVGestionarEDT')
